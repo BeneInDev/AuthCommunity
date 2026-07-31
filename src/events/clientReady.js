44c811a569f3209def5929cfff191b55a3fca857
BeneInDev <ricardopestana2025@gmail.com>
2026-07-30 21:17:55 -0300

    Update clientReady.js

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -1,20 +1,20 @@
-const { ActivityType, Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
+
     execute(client) {
-        console.log(`Logged in as ${client.user.tag}!`);
-        console.log(`Bot ID: ${client.user.id}`);
-        console.log(`Connected to ${client.guilds.cache.size} guilds`);
+        console.log(`✅ ${client.user.tag} online`);
 
         client.user.setPresence({
-            activities: [{ 
-                name: 'Pure Solutions', 
-                type: ActivityType.Streaming,
-                url: 'https://twitch.tv/puresolutions'
-            }],
-            status: 'online',
+            activities: [
+                {
+                    name: 'Community Games • discord.gg/mN78zKpWja',
+                    type: ActivityType.Playing
+                }
+            ],
+            status: 'online'
         });
-    },
+    }
 };
